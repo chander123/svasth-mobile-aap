e734cbd6e34c79911b726e30560898171d30ddc7
chander123 <chanderbhushan8819@gmail.com>
2018-01-19 11:56:27 +0530

    added send data to server code

--- svasthapp/svasthapp/www/js/index.js
+++ svasthapp/svasthapp/www/js/index.js
@@ -16,8 +16,14 @@
 /* global detailPage, resultDiv, messageInput, sendButton, disconnectButton */
 /* global cordova, bluetoothSerial  */
 /* jshint browser: true , devel: true*/
-'use strict';
 
+var mobile_num;
+var bluetoothDeviceName;
+var msg = 'none';
+var lat;
+var long;
+var currentLocation;
+'use strict';
 var app = {
     initialize: function() {
         this.bindEvents();
@@ -34,8 +40,23 @@ var app = {
         sendButton.addEventListener(TOUCH_START, this.sendData, false);
         disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
         deviceList.addEventListener('touchstart', this.connect, false);
+
     },
     onDeviceReady: function() {
+        window.plugins.sim.hasReadPermission(success, error);
+        window.plugins.sim.requestReadPermission(success, error);
+        window.plugins.sim.getSimInfo(successCallback, errorCallback);
+        cordova.plugins.locationAccuracy.request(function (success){
+            console.log("Successfully requested accuracy: "+success.message);
+            }, function (error){
+                console.error("Accuracy request failed: error code="+error.code+"; error message="+error.message);
+            window.location.reload(true);
+            if(error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED){
+                if(window.confirm("Failed to automatically set Location Mode to 'High Accuracy'. Would you like to switch to the Location Settings page and do this manually?")){
+                    cordova.plugins.diagnostic.switchToLocationSettings();
+                }
+            }
+        }, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
         app.refreshDeviceList();
     },
     refreshDeviceList: function() {
@@ -47,9 +68,8 @@ var app = {
         // remove existing devices
         deviceList.innerHTML = "";
         app.setStatus("");
-
         devices.forEach(function(device) {
-
+            bluetoothDeviceName = device.id;
             var listItem = document.createElement('li'),
                 html = '<b>' + device.name + '</b><br/>' + device.id;
 
@@ -84,7 +104,7 @@ var app = {
         } else {
             app.setStatus("Found " + devices.length + " device" + (devices.length === 1 ? "." : "s."));
         }
-
+        //navigator.geolocation.getCurrentPosition(onSuccess, onError);
     },
     connect: function(e) {
         var onConnect = function() {
@@ -97,6 +117,7 @@ var app = {
             };
 
         var deviceId = e.target.dataset.deviceId;
+        bluetoothDeviceName = deviceId;
         if (!deviceId) { // try the parent
             deviceId = e.target.parentNode.dataset.deviceId;
         }
@@ -107,6 +128,8 @@ var app = {
         console.log(data);
         resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + data + "<br/>";
         resultDiv.scrollTop = resultDiv.scrollHeight;
+        msg = msg +','+ data;
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     },
     sendData: function(event) { // send data to Arduino
 
@@ -148,5 +171,72 @@ var app = {
     },
     onError: function(reason) {
         alert("ERROR: " + reason); // real apps should use notification.alert
+    },
+    sendDataToServer: function() {
+        if(msg == 'none') { msg = '';}
+        jQuery.ajax ({
+            type: 'POST',
+            async: false,
+            url: 'http://www.miisky.com/svasth_insert.php',
+            data: 'message='+encodeURIComponent(msg)+'&mac_no='+encodeURIComponent(bluetoothDeviceName)+'&diabetes=114&sim_no='+mobile_num+'&location='+encodeURIComponent(currentLocation),
+            error: function(e) {
+              alert('Please try again, if problem persists contact the administrator.');
+            },
+            beforeSend: function() {
+                console.info('sending data...');
+            },
+            success: function(response) {
+              console.log(response);
+            }
+        });
     }
+
 };
+
+function successCallback(result) {
+    mobile_num = result.deviceId;
+}
+
+function errorCallback(error) {
+    console.log(error);
+}
+
+function success(result) {
+    console.log(result);
+}
+
+function error(error) {
+    console.log(error);
+}
+
+function onSuccess(position) {
+    lat  = position.coords.latitude;
+    long = position.coords.longitude;
+    displayLocation(lat,long);
+};
+
+// onError Callback receives a PositionError object
+function onError(error) {
+    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+}
+
+
+function displayLocation(latitude,longitude){
+    var request = new XMLHttpRequest();
+
+    var method = 'GET';
+    var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
+    var async = true;
+
+    request.open(method, url, async);
+    request.onreadystatechange = function(){
+      if(request.readyState == 4 && request.status == 200){
+        var data = JSON.parse(request.responseText);
+        var address = data.results[0];
+        currentLocation = address.formatted_address;
+        app.sendDataToServer();
+      }
+    };
+    request.send();
+}
+
